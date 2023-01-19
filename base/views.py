e4501c9ac1a7a25c5d568370630f2c070a9d64a9
Marrion7 <marrionhodonou@gmail.com>
2023-01-19 21:56:05 +0400

    second commit

--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
+class TaskList(ListView):
+    model = Task
