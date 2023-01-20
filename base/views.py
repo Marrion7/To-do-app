fd7df927753204535f42a04fcc719646b8dc74b1
Marrion7 <marrionhodonou@gmail.com>
2023-01-20 20:34:25 +0400

    third commit

--- base/views.py
+++ base/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreateView(CreateView):
+    model = Task    
